2ac0894d5f88ff27d78a78743449c31cbf32a8f6
Rodriguetech <rodriguet802@gmail.com>
2022-02-08 10:55:19 +0100

    Get user solde

---
+++ src/Goxens.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Goxens;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+
+/**
+ * Class Goxens
+ * @package Goxens
+ */
+class Goxens
+{
+    // Account api key
+    private  $apiKey;
+
+    // User uid
+    private  $uid;
+
+    private $curl;
+
+
+
+    /**
+     * Goxens constructor.
+     * @param $apiKey
+     * @param $uid
+     */
+    public function __construct($apiKey, $uid)
+    {
+        $this->apiKey = $apiKey;
+        $this->uid = $uid;
+        $this->curl = new Client();
+    }
+
+
+
+
+
+    public function verifySolde($apikey){
+        $response = null;
+
+        try {
+            $endpoint = Constants::BASE_URL ;
+
+
+            $response = $this->curl->request('GET', $endpoint . '/api/credit/'. $apikey, [
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Content-Type' => 'application/json',
+                ],
+            ]);
+
+           return  $response->getBody()->getContents();
+
+        }catch (RequestException $e){
+            $body = ($e->getResponse()->getBody());
+
+            $errors = (json_decode((string)$body));
+
+            $errors->statusCode = $e->getResponse()->getStatusCode();
+
+            return $errors;
+        }
+
+    }
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @return mixed
+     */
+
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+}
