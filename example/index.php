b895f2752d40a2cec40f38a659a0612b52ced0c3
Rodriguetech <rodriguet802@gmail.com>
2022-02-08 11:38:49 +0100

    Update example

--- example/index.php
+++ example/index.php
@@ -7,14 +7,29 @@ require '../vendor/autoload.php';
 
 include_once '../src/Goxens.php';
 
+# Initialize the Goxens class
 
 $apiKey = 'YOUR_API_KEY';
 $userUid = 'YOUR_USER_ID';
 
+# Create a new Goxens instance
 
 $goxens =  new Goxens($apiKey, $userUid);
 
+############### Get user solde ###############
+
 $solde = $goxens->verifySolde($apiKey);
 
-
 var_dump($solde);
+
+
+
+####################### send SMS ##############################
+
+$sender = 'Goxens'; // Valid sender name
+$number = '+229XXXXXXXX';
+$message = 'Bienvenue sur Goxens';
+
+$send = $goxens->sendSms($apiKey,$userUid,$number,$sender,$message);
+
+var_dump($send);
